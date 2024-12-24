24a1f6b8b148db4f6df54ba4684050f179d1ed07
L_Sowmya <112118575+LakshmiSowmya04@users.noreply.github.com>
2024-12-24 12:29:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-<<<<<<< HEAD
-import logo from "./logo.svg";
-import { Router, Routes, Route } from "react-dom";
-import "./App.css";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-import Send from "./components/send";
-=======
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Send from "./components/Send";
->>>>>>> bd411f2da4b6234f64ff27eaaa9913fe18d9debe
+import { useState } from "react";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/send" element={<Send />} />
-      </Routes>
-    </Router>
-  );
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <Router>
+            <Routes>
+                <Route 
+                    path="/" 
+                    element={<Login setIsLoggedIn={setIsLoggedIn} />} 
+                />
+                <Route 
+                    path="/signup" 
+                    element={<Signup setIsLoggedIn={setIsLoggedIn} />} 
+                />
+                <Route 
+                    path="/send" 
+                    element={
+                        isLoggedIn ? <Send /> : <Login setIsLoggedIn={setIsLoggedIn} />
+                    } 
+                />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
